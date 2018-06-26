709ca623bcefd76b16251c5d19c233487eee4fbb
Albert Sarlé <albertsarle@gmail.com>
2018-06-26 12:48:11 +0200

    fixes index.php example

--- index.php
+++ index.php
@@ -4,17 +4,18 @@ use Sarle\Tennis\Match;
 
 require "vendor/autoload.php";
 
-$this->match = new Match('Jugador 1', 'Jugador 2');
+$match = new Match('Jugador 1', 'Jugador 2');
 
-$this->match->newPointFor('Jugador 1');
-$this->match->newPointFor('Jugador 2'); // 15 - 15
-$this->match->newPointFor('Jugador 1');
-$this->match->newPointFor('Jugador 2'); // 30 - 30
-$this->match->newPointFor('Jugador 1'); // 40 -30
-$this->match->newPointFor('Jugador 2'); // Deuce
-$this->match->newPointFor('Jugador 1'); // ADVANTAGE Jugador 1
-$this->match->newPointFor('Jugador 2'); // Deuce
-$this->match->newPointFor('Jugador 2'); // ADVANTAGE Jugador 2
-$this->match->newPointFor('Jugador 2'); // Jugador 2 WINS
 
-echo $this->match->getScoring();
+$match->newPointFor('Jugador 1');
+$match->newPointFor('Jugador 2'); // 15 - 15
+$match->newPointFor('Jugador 1');
+$match->newPointFor('Jugador 2'); // 30 - 30
+$match->newPointFor('Jugador 1'); // 40 -30
+$match->newPointFor('Jugador 2'); // Deuce
+$match->newPointFor('Jugador 1'); // ADVANTAGE Jugador 1
+$match->newPointFor('Jugador 2'); // Deuce
+$match->newPointFor('Jugador 2'); // ADVANTAGE Jugador 2
+$match->newPointFor('Jugador 2'); // Jugador 2 WINS
+
+$this->assertEquals('Jugador 2 WINS', $match->getScoring() );
